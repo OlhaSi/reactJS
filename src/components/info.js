b4d4aecd47bca665e4d2769862a6907ac69d6c17
OlhaSi <liush.olga@gmail.com>
2021-04-14 22:58:56 +0200

    feat: add components structure

---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Weather checker app</h2>
+                <p>Check the weather in your city!</p>
+            </div>
+        );
+    }
+}
+
+export default Info;
